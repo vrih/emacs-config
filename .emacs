0a35b6042747654abbf6c7c9bc6a88311d98c7c6
Daniel Bowtell <dan@danbowtell.co.uk>
2014-09-01 09:16:14 +0100

    update

--- .emacs
+++ .emacs
@@ -30,7 +30,7 @@
 
 ;;graphics
 ( set-face-attribute 'default nil :font "Liberation Mono-10")
-(load-theme 'monokai)
+(load-theme 'monokai t)
 
 ;disable backup
 (setq backup-inhibited t)
@@ -57,6 +57,7 @@
         vrih-magit
         vrih-helm
         vrih-python
+        vrih-mu4e
         vrih-ido))
 
 (dolist (file vrih-pkg-full)
@@ -115,10 +116,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" default))))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
--- vrih/vrih-mu4e.el
+++ vrih/vrih-mu4e.el
@@ -1,10 +1,23 @@
 (require 'mu4e)
 
-(setq mu4e-maildir "~/.fastmail_maildir")
-(setq mu4e-drafts-folder "/INBOX.Drafts")
-(setq mu4e-trash-folder "/INBOX.Trash")
-(setq mu4e-sent-folder "/INBOX.Sent Items")
-(setq mu4e-refile-folder "/INBOX.Archive")
+(if (string-equal (system-name) "daniel-Latitude-3440")
+    (setq
+     mu4e-maildir "~/Maildir"
+     mu4e-drafts-folder "/[Gmail].Drafts"
+     mu4e-trash-folder "/[Gmail].Bin"
+     mu4e-sent-folder "/[Gmail].Sent Mail"
+     mu4e-refile-folder "/[Gmail].All Mail"
+     mu4e-headers-skip-duplicates t
+     mu4e-headers-fields '((:human-date . 10)
+                           (:flags . 6)
+                           (:maildir . 15)
+                           (:from . 25)
+                           (:subject . nil)))
+  (setq mu4e-maildir "~/.fastmail_maildir"
+        mu4e-drafts-folder "/INBOX.Drafts"
+        mu4e-trash-folder "/INBOX.Trash"
+        mu4e-sent-folder "/INBOX.Sent Items"
+        setq mu4e-refile-folder "/INBOX.Archive"))
 
 (setq mu4e-msg2pdf "/usr/bin/msg2pdf")
 
@@ -13,6 +26,18 @@
    (when (fboundp 'imagemagick-register-types)
       (imagemagick-register-types))
 
+(setq mu4e-sent-messages-behavior 'delete)
+(setq mu4e-maildir-shortcuts
+    '( ("/INBOX"               . ?i)
+       ("/[Gmail].Sent Mail"   . ?s)
+       ("/CCd"   . ?c)
+       ("/[Gmail].Trash"       . ?t)
+       ("/[Gmail].All Mail"    . ?a)))
+
 (setq mu4e-get-mail-command "offlineimap")
+(setq mu4e-update-interval 300)
+
+(global-set-key (kbd "<f7>") 'mu4e)
 
 (require 'smtpmail)
+(provide 'vrih-mu4e)
