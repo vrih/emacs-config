a4892ba7f7f689d69126e5eab602f3ecd224c18d
Daniel Bowman <dan@danbowtell.co.uk>
2018-03-05 19:24:47 +0000

    Update configs

--- vrih/vrih-restclient.el
+++ vrih/vrih-restclient.el
@@ -1,6 +1,7 @@
 (use-package restclient
   :ensure t
-  :init (add-to-list 'auto-mode-alist '("\\.rcl\\'" . restclient-mode)))
+  :init (add-to-list 'auto-mode-alist '("\\.rcl\\'" . restclient-mode))
+  :mode (("\\.rcl" . restclient-mode)))
 
 (use-package company-restclient
   :init (add-to-list 'company-backends 'company-restclient)
