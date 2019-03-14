3b31c84c884739b5004fe42858ce92021ba753c7
Daniel Bowman <daniel.bowman@blacksquaremedia.com>
2019-03-14 11:07:23 +0000

    Format TF buffers on save

--- vrih/vrih-terraform.el
+++ vrih/vrih-terraform.el
@@ -4,11 +4,14 @@
   :bind (("C-c c" . (lambda () (interactive) (shell-command "make plan"))))
   :mode ("\\.tf\\'" . terraform-mode)
   :init (setq projectile-project-test-cmd  "make plan")
+  (setq terraform-format-on-save-mode 1)
   (setq projectile-project-run-cmd "make apply"))
 
 (use-package company-terraform
   :ensure t
   :defer t
   :init (company-terraform-init)) 
+
+(add-hook 'terraform-mode-hook 'terraform-format-on-save-mode)
 
 (provide 'vrih-terraform)
