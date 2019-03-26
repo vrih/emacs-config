9b8006284a37b153fb7def3773a1e7fb36659a42
Daniel Bowman <daniel.bowman@blacksquaremedia.com>
2019-03-26 11:25:37 +0000

    Add shortcut for faster diff evaluation

--- vrih/vrih-magit.el
+++ vrih/vrih-magit.el
@@ -1,12 +1,18 @@
 (use-package magit
   :bind (("C-x g" . magit-status))
-  :ensure t)
+  :ensure t
+  :init (add-hook 'magit-diff-mode-hook
+                  (lambda () (local-set-key (kbd "<M-RET>")
+                                       'magit-diff-visit-file-other-window))))
 
 (use-package git-gutter
   :ensure t
   :config (global-git-gutter-mode))
 
 (use-package gist
+  :ensure t)
+
+(use-package magit-gitflow
   :ensure t)
 
 (provide 'vrih-magit)
