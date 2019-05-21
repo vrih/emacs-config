a481b52a59d96626eb810a3d996d80d528b34c42
Daniel Bowman <daniel.bowman@blacksquaremedia.com>
2019-05-21 09:17:01 +0100

    Update vuejs hydra

--- vrih/vrih-vue.el
+++ vrih/vrih-vue.el
@@ -7,8 +7,8 @@
                
    ("t" pojectile-find-test-file)
    ("s" rspec-verify-single)
-   ("f" rspec-verify)
-   ("a" (lambda nil (interactive) (compile "yarn test")))
+   ("f" (lambda nil (interactive) (let ((default-directory (projectile-project-root))) (compile (concat "yarn test:unit --no-cache  " buffer-file-name)))))
+   ("a" (lambda nil (interactive) (compile "yarn test:unit --no-cache")))
    ("r" (lambda nil (interactive) (compile "yarn lint --fix")))
    ("R" (lambda nil (interactive) (compile "yarn lint"))))
 
@@ -31,7 +31,13 @@
               (setq company-minimum-prefix-length 0)
               (flycheck-mode 1)
               (local-set-key  (kbd "C-c r") 'hydra-vue/body)
-              (push '("=>" . ?⇒) prettify-symbols-alist))))
+              (push '("=>" . ?⇒) prettify-symbols-alist)))
+  (add-hook 'js-mode-hook
+            (lambda ()
+              (flycheck-mode 1)
+              (local-set-key  (kbd "C-c r") 'hydra-vue/body)
+              (push '("=>" . ?⇒) prettify-symbols-alist)))
+  )
 
 (add-to-list 'load-path "~/.emacs.d/local/company-vuetify")
 (use-package company-vuetify
